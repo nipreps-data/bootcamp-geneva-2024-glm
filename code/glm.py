2c86d641875bf2aab6a8de601029e6efa412c410
Chris Markiewicz <effigies@gmail.com>
2024-09-05 10:54:48 -0400

    Start GLM code by loading dataset, need to wait on BOLD data to continue

---
+++ code/glm.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env -S uv run
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "numpy",
+#     "nibabel",
+#     "niworkflows",
+#     "nilearn",
+#     "pybids",
+# ]
+# ///
+import bids
+import numpy as np
+import nibabel as nb
+import nilearn as nl
+import niworkflows as nw
+import niworkflows.data
+
+layout = bids.BIDSLayout("sourcedata/raw")
+layout.add_derivatives(
+    "sourcedata/derivatives/fmriprep-24.0.0",
+    config=[nw.data.load("nipreps.json")],
+    validate=False,
+)
+
+bold_files = layout.get(suffix="bold", desc="preproc", extension=".nii.gz")
+events = layout.get_collecctions(level='run')
+
